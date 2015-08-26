7deaf34bd21d2b935fcbbbe7ed87ff027988a2a2
werdbrian <werdbrian@gmail.com>
2015-08-25 20:33:51 -0700

    Update Program.cs

--- autolevel/Program.cs
+++ autolevel/Program.cs
@@ -161,7 +161,7 @@ namespace AutoLevelup
                     tipo = " AP";
                 }
             }
-            else if (Player.ChampionName  == "Leblanc") abilitySequence = new int[] { 1, 2, 3, 1, 1, 4, 1, 2, 1, 3, 4, 3, 3, 3, 2, 4, 2, 2 };
+            else if (Player.ChampionName  == "Leblanc") abilitySequence = new int[] { 2, 1, 3, 2, 2, 4, 2, 1, 2, 1, 4, 1, 1, 3, 3, 4, 3, 3 };
             else if (Player.ChampionName  == "LeeSin")
             {
                 if (Smite != SpellSlot.Unknown)
